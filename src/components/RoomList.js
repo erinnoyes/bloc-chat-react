4a75b5cbc9badd0abc9c3172ceb5def7b61d29d2
Erin Noyes <31861984+erinnoyes@users.noreply.github.com>
2018-01-18 18:09:56 -0800

    Checkpoint 3 completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,10 +4,23 @@ export class RoomList extends Component {
   constructor(props) {
     super(props);
       this.state = {
+        name: "",
         rooms: []
       };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleChange = this.handleChange.bind(this);
+    this.createRoom = this.createRoom.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    this.roomsRef.push({ name: this.state.name });
+    this.setState({ name: "" });
   }
 
   componentDidMount() {
@@ -19,11 +32,23 @@ export class RoomList extends Component {
   }
 
   render() {
+    const  addForm = (
+      <form onSubmit={this.createRoom}>
+        <input type="text" value={this.state.name} placeholder="Add Room Name" onChange={this.handleChange}/>
+        <input type="submit" value="Create" />
+      </form>
+);
+
     const roomList = this.state.rooms.map((room) =>
       <li key={room.key}>{room.name}</li>
     );
+
     return(
-      <ul>{roomList}</ul>
+      <div>
+        <div>{addForm}</div>
+        <ul>{roomList}</ul>
+      </div>
+
     );
   }
 }
